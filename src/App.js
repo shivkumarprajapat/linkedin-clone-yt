2c187dd9e7fd1dc6dda011ad8179b3fe490ab0ee
ShivKumar <75515360+shivkumarprajapat@users.noreply.github.com>
2021-12-18 22:00:55 +0530

    Login functionality Added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import './App.css';
+import Feed from './components/Body/Feed/Feed';
+import Sidebar from './components/Body/Sidebar/Sidebar';
+import Widgets from './components/Body/Widget/Widgets';
 import Header from './components/Header/Header';
-import Sidebar from './components/Sidebar/Sidebar';
-import Feed from './Feed'
-const App = () => {
+import Login from './components/Login/Login';
+import { login, logout, selectUser } from './features/userSlice';
+import { auth } from './firebase';
+
+function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        // user is logged in
+        dispatch(login({
+          email: userAuth.email,
+          uid: userAuth.uid,
+          displayName: userAuth.displayName,
+          photoUrl: userAuth.photoURL,
+        }))
+      } else {
+        // use is logged out
+        dispatch(logout());
+      }
+    })
+  }, [dispatch]);
+
   return (
     <div className="app">
       <Header />
 
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app_body">
+          <Sidebar />
+          <Feed />
+          <Widgets />
+        </div>
+      )}
+      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,13 @@
-import firebase from "firebase";
+import firebase from 'firebase'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD4XvC-9b6CbMs6g3_ZMTVEJnq5yO6Cw3c",
-    authDomain: "linkedin-clone-5897a.firebaseapp.com",
-    projectId: "linkedin-clone-5897a",
-    storageBucket: "linkedin-clone-5897a.appspot.com",
-    messagingSenderId: "674948610586",
-    appId: "1:674948610586:web:f3a275546af4b97e952fa3"
+  apiKey: "AIzaSyD4XvC-9b6CbMs6g3_ZMTVEJnq5yO6Cw3c",
+  authDomain: "linkedin-clone-5897a.firebaseapp.com",
+  projectId: "linkedin-clone-5897a",
+  storageBucket: "linkedin-clone-5897a.appspot.com",
+  messagingSenderId: "674948610586",
+  appId: "1:674948610586:web:f3a275546af4b97e952fa3"
 };
-
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
